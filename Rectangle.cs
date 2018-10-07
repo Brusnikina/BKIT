71168aedc5cdd27b51593ac552c2c764a8514ee6
Brusnikina <43144646+Brusnikina@users.noreply.github.com>
2018-10-08 02:38:59 +0300

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Rectangle : Figure, IPrint
+    {  
+        public double height { get; protected set; }
+        public double width { get; protected set; }
+        
+        public Rectangle(double h, double w)
+        {
+            this.height = h;
+            this.width = w;
+            this.Type = "Прямоугольник";
+        }
+        public override double Area()
+        {
+            return this.height * this.width;
+        }
+        public void Print()
+        {
+            Console.WriteLine(this.ToString()); 
+        }
+
+    }
+}
